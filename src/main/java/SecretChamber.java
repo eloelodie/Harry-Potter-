2562af395775cd34211eb886c57b221b5e2bd89e
eloelodie <120282774+eloelodie@users.noreply.github.com>
2023-03-30 21:29:46 +0200

    Update SecretChamber.java

--- src/main/java/SecretChamber.java
+++ src/main/java/SecretChamber.java
@@ -8,26 +8,26 @@ public class SecretChamber implements LevelAction {
     @Override
     public void performAction(Wizard wizard, ArrayList<Enemy> enemies, ArrayList<Boss> bosses) {
         String enemyName = enemies.get(0).getName();
-        TU.printHeading("Vous entrez dans la Chambre des secrets.");
+        TU.printHeading("You enter the Chamber of Secrets.");
         if (wizard.getHouse() == House.GRYFFINDOR) {
-            TU.printConsole("Vous trouvez l'épée de Gryffondor et venez à bout du Basilic !");
+            TU.printConsole("You find Gryffindor's sword and defeat the Basilisk !");
             sk.specialDeath(enemyName, enemies.get(0));
         }
         while (wizard.getHp() > 0 && enemies.get(0).getHp() > 0) {
-            TU.printConsole("Vous pouvez essayer de lui arracher un de ses crocs avec Accio.");
-            switch (TU.askInt("Que voulez-vous faire ? \n(1) Attaquer \n(2) Utiliser Accio\n--> ", 2)) {
+            TU.printConsole("You can try to rip one of its fangs out with Accio.");
+            switch (TU.askInt("What do you want to do ? \n(1) Attack \n(2) Use Accio\n--> ", 2)) {
                 case 1 -> {
                     wizard.attack(bosses.get(0), wizard.getDamage());
                     basiliskAttack(wizard, enemies.get(0));
                 }
                 case 2 -> {
                     if (Math.random() * 100 <= 60 && wizard.getKnownSpells().contains(sk.getSpells()[4])) {
-                        TU.printConsole("Vous avez réussi à lui arracher un croc !");
+                        TU.printConsole("You managed to pull a fang out of him !");
                         sk.specialDeath(enemyName, enemies.get(0));
                         sk.learningSpell(wizard, sk.getSpells()[2]);
-                        TU.printConsole("Vous utilisez le croc pour détruire le journal de Tom Jedusor !");
+                        TU.printConsole("You use the fang to destroy Tom Riddle's journal !");
                     } else {
-                        TU.printConsole("Vous avez raté votre sort !");
+                        TU.printConsole("You missed your spell !");
                         wizard.getKnownSpells().get(1).castSpell(wizard, enemies.get(0));
                         basiliskAttack(wizard, enemies.get(0));
                     }
@@ -37,9 +37,9 @@ public class SecretChamber implements LevelAction {
     }
 
     public void basiliskAttack(Wizard wizard, Enemy enemy) {
-        TU.printConsole("Le " + enemy.getName() + " vous attaque !");
+        TU.printConsole("the " + enemy.getName() + " attacks you !");
         enemy.attack(wizard, enemy.getDamage());
-        TU.printConsole("Il vous reste " + wizard.getHp() + " points de vie.");
+        TU.printConsole("You still have " + wizard.getHp() + "life points.");
     }
 
 }
