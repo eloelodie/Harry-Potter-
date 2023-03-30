7bcc5c3e860fcb1077e5ef863c9855056096771f
eloelodie <120282774+eloelodie@users.noreply.github.com>
2023-03-30 21:26:19 +0200

    Update DungeonsToilets.java

--- src/main/java/DungeonsToilets.java
+++ src/main/java/DungeonsToilets.java
@@ -8,22 +8,22 @@ public class DungeonsToilets implements LevelAction {
     @Override
     public void performAction(Wizard wizard, ArrayList<Enemy> enemies, ArrayList<Boss> bosses) {
         String bossName = bosses.get(0).getName();
-        TU.printHeading("Vous entrez dans les toilettes du dongeons");
-        TU.printConsole("Un " + bossName + " apparait !");
+        TU.printHeading("You enter the dungeon toilets");
+        TU.printConsole("A " + bossName + " appears !");
         while (wizard.getHp() > 0 && bosses.get(0).getHp() > 0) {
-            TU.printConsole("Vous pouvez essayer de faire tomber la massue sur sa tête avec Wingardium Leviosa.");
-            switch (TU.askInt("Que voulez-vous faire ? \n(1) Attaquer \n(2) Utiliser Wingardium Leviosa\n--> ", 2)) {
+            TU.printConsole("You can try knocking the club on its head with Wingardium Leviosa.");
+            switch (TU.askInt("What do you want to do ? \n(1) Attack \n(2) Use Wingardium Leviosa\n--> ", 2)) {
                 case 1 -> {
                     wizard.attack(bosses.get(0), wizard.getDamage());
                     trollAttack(wizard, bosses.get(0));
                 }
                 case 2 -> {
                     if (Math.random() * 100 <= 60 && wizard.getKnownSpells().contains(sk.getSpells()[0])) {
-                        TU.printConsole("Vous avez réussi à la faire tomber sur la tête du " + bossName + " !");
+                        TU.printConsole("You managed to make it fall on the head of the " + bossName + " !");
                         sk.specialDeath(bossName, bosses.get(0));
                         sk.learningSpell(wizard, sk.getSpells()[4]);
                     } else {
-                        TU.printConsole("Vous avez raté votre sort !");
+                        TU.printConsole("You missed your spell !");
                         wizard.getKnownSpells().get(0).castSpell(wizard, bosses.get(0));
                         trollAttack(wizard, bosses.get(0));
                     }
@@ -34,10 +34,11 @@ public class DungeonsToilets implements LevelAction {
     }
 
     public void trollAttack(Wizard wizard, Boss boss) {
-        TU.printConsole("Le " + boss.getName() + " vous attaque avec sa massue !");
+        TU.printConsole("The " + boss.getName() + " attacks you with his club !");
         boss.attack(wizard, boss.getDamage());
-        TU.printConsole("Il vous reste " + wizard.getHp() + " points de vie.");
-        TU.printConsole("La pierre est bancale...");
+        TU.printConsole("You still have " + wizard.getHp() + " life points .");
+        TU.printConsole("The stone is wobbly...");
     }
 
 }
+
